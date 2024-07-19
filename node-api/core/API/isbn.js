e45eee48204d3bbc89f0214ccbea6146e3484074
10Dev <47715101+Patiyut1807@users.noreply.github.com>
2024-07-20 01:18:00 +0700

    Update isbn.js

--- node-api/core/API/isbn.js
+++ node-api/core/API/isbn.js
@@ -2,7 +2,7 @@ import { default as axios } from 'axios';
 
 const IsdnBookDetails = async (isbnKey) => {
   let res = await axios.get("https://api2.isbndb.com/book/"+isbnKey,{
-    headers: {"Authorization": '49994_49890d11b9afe71fb00be4ff35ad2893',"Accept":"*/*"},
+    headers: {"Authorization": 'key ที่หายไป',"Accept":"*/*"},
   });
   console.log(res.data);
 };
